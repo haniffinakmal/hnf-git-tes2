64fc7ff16cf674d29861aae6adcd96375bfc80e0
akmal Haniffin <haniffinakmal@gmail.com>
2020-04-25 15:30:01 +0700

    menambahkan list

---
+++ index.php
@@ -0,0 +1,17 @@
+<DOCTYPE html>
+<html lang="en"
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width",
+	<meta http-equiv="X-UA-Compatible"> content="ie=edge"
+<title>Coba git remote</title>
+<head>
+<body>
+	<h1>Hello world!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem pariatur minus quod explicabo ullam maiores ut deleniti magni, sed illum deserunt, qui quidem dignissimos maxime iure quos perspiciatis neque et.</p>  
+<ul>
+    <li>satu</li>
+    <li>dua</li>
+</ul>
+<body>
+<html>
